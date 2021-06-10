13c7081af6e494c9550ba06718331a5097832876
sinzii <thangvx@newayict.com>
2021-06-10 15:16:03 +0700

    Update footer & button component

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+const footerLinks = require('./data/footerLinks.json');
+
 module.exports = {
     baseUrl: '',
     baseContext: '',
@@ -6,33 +8,5 @@ module.exports = {
     dateTimeFormat: 'ddd, MM/DD/YYYY - HH:mm',
     dateFormat: 'MM/DD/YYYY',
     postUrlStyle: 'POSTS_SLUG',
-    me: {
-        fullname: 'Your Name',
-        nickname: '@meblog',
-        github: 'https://github.com/sinzii/meblog',
-    },
-    whatToDoNext: [
-        {
-            url: 'https://github.com/sinzii/meblog#template-project-structure',
-            text: 'Design the new site',
-        },
-        {
-            url: 'https://github.com/sinzii/meblog#how-to-create-new-post',
-            text: 'Compose new posts',
-        },
-        {
-            url: 'https://github.com/sinzii/meblog#deploy-your-site-on-github',
-            text: 'Deploy your site to Github Pages',
-        },
-    ],
-    websitesBuiltWithMeblog: [
-        {
-            url: 'https://meblog.sinzii.me',
-            text: 'meblog website',
-        },
-        {
-            url: 'https://sinzii.me',
-            text: 'Thang X. Vu (@sinzii)',
-        },
-    ]
+    footerLinks
 };
