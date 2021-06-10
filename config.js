bc097c23ba7a132ef052d30f32f25a79322aad12
sinzii <thangvx@newayict.com>
2021-06-10 14:41:46 +0700

    Init project with index page

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    baseUrl: '',
+    baseContext: '',
+    siteName: 'HydraDX',
+    siteDescription: 'Cross-chain liquidity protocol built on Substrate',
+    dateTimeFormat: 'ddd, MM/DD/YYYY - HH:mm',
+    dateFormat: 'MM/DD/YYYY',
+    postUrlStyle: 'POSTS_SLUG',
+    me: {
+        fullname: 'Your Name',
+        nickname: '@meblog',
+        github: 'https://github.com/sinzii/meblog',
+    },
+    whatToDoNext: [
+        {
+            url: 'https://github.com/sinzii/meblog#template-project-structure',
+            text: 'Design the new site',
+        },
+        {
+            url: 'https://github.com/sinzii/meblog#how-to-create-new-post',
+            text: 'Compose new posts',
+        },
+        {
+            url: 'https://github.com/sinzii/meblog#deploy-your-site-on-github',
+            text: 'Deploy your site to Github Pages',
+        },
+    ],
+    websitesBuiltWithMeblog: [
+        {
+            url: 'https://meblog.sinzii.me',
+            text: 'meblog website',
+        },
+        {
+            url: 'https://sinzii.me',
+            text: 'Thang X. Vu (@sinzii)',
+        },
+    ]
+};
